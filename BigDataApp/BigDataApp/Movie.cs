a2f326a879d9cb0d764888bacf137bc9ff1d0332
Semyon Khechnev <hechnev@gmail.com>
2022-10-01 18:49:30 +0300

    completed task a

--- BigDataApp/BigDataApp/Movie.cs
+++ BigDataApp/BigDataApp/Movie.cs
@@ -10,7 +10,7 @@ namespace BigDataApp
     {
         public string Title { get; set; }
         public HashSet<string> Actors { get; set; }
-        public string Directors { get; set; }
+        public HashSet<string> Directors { get; set; }
         public HashSet<string> Tags { get; set; }
         public string Rating { get; set; }
     }
--- BigDataApp/BigDataApp/Program.cs
+++ BigDataApp/BigDataApp/Program.cs
@@ -11,7 +11,7 @@ class Program
                         .GroupBy(item => item[0])
                         .Select(group => new { Id = group.Key, FilmsInfo = group.Select(filmLine => new { Title = filmLine[2], Language = filmLine[4] }) })
                         .Where(x => x.FilmsInfo.ToList().Where(y => y.Language == "ru" || y.Language == "en").Any())
-                        .ToDictionary(x => x.Id, x => x.FilmsInfo.Select(x => x.Title).ToList());
+                        .ToDictionary(x => x.Id, x => x.FilmsInfo.Where(x => x.Language == "ru" || x.Language == "en").Select(x => x.Title).Distinct().ToList());
 
         string pathActorsDirectorsNames = @"C:\Users\s-khechnev\Desktop\ml-latest\ActorsDirectorsNames_IMDB.txt";
         string pathActorsDirectorsCodes = @"C:\Users\s-khechnev\Desktop\ml-latest\ActorsDirectorsCodes_IMDB.tsv";
@@ -25,7 +25,16 @@ class Program
             .Where(x => movieCodesDict.ContainsKey(x[0]))
             .GroupBy(x => new { FilmId = x[0] })
             .Select(group => new { Key = group.Key, CategoryGroup = group.GroupBy(x => x[3]) })
-            .ToDictionary(x => x.Key.FilmId, x => x.CategoryGroup.ToDictionary(y => y.Key, y => y.Select(z => actorsDirectorsNamesDict[z[2]]).ToList()));
+            .ToDictionary(x => x.Key.FilmId,
+                          x => x.CategoryGroup.ToDictionary(y => y.Key, y =>
+                              /*y.Select(z => *//*actorsDirectorsNamesDict[*//*z[2]).ToList()*/
+                              /*y.Select(z => new { PersonId = z[2], IsKnown = actorsDirectorsNamesDict.ContainsKey(z[2]) })///cringe
+                              .Where(x => x.IsKnown)
+                              .Select(x => actorsDirectorsNamesDict[x.PersonId])
+                              .ToList()*/
+                              y.Select(z => actorsDirectorsNamesDict.ContainsKey(z[2]) ? actorsDirectorsNamesDict[z[2]] : z[2])
+                              .ToList()
+                          ));
 
         string pathRating = @"C:\Users\s-khechnev\Desktop\ml-latest\Ratings_IMDB.tsv";
 
@@ -58,16 +67,68 @@ class Program
         {
             foreach (var item in movieCodesDict[key])
             {
+                var title = item;
+                string raiting = String.Empty;
+                HashSet<string> directors = null;
+                HashSet<string> tags = null;
+                HashSet<string> actors = null;
+
+                if (ans1.ContainsKey(item))//cringe
+                    continue;
+
+                if (raitingDict.ContainsKey(key))
+                {
+                    raiting = raitingDict[key];
+                }
+                if (actorsDirectorsCodesDict.ContainsKey(key))
+                {
+                    ///cringe
+                    if (actorsDirectorsCodesDict[key].ContainsKey("actor"))
+                    {
+                        if (actors != null)
+                            actors.Union(actorsDirectorsCodesDict[key]["actor"].ToHashSet());
+                        else
+                            actors = actorsDirectorsCodesDict[key]["actor"].ToHashSet();
+                    }
+
+                    if (actorsDirectorsCodesDict[key].ContainsKey("actress"))
+                    {
+                        if (actors != null)
+                            actors.Union(actorsDirectorsCodesDict[key]["actress"].ToHashSet());
+                        else
+                            actors = actorsDirectorsCodesDict[key]["actress"].ToHashSet();
+                    }
+
+                    if (actorsDirectorsCodesDict[key].ContainsKey("self"))
+                    {
+                        if (actors != null)
+                            actors.Union(actorsDirectorsCodesDict[key]["self"].ToHashSet());
+                        else
+                            actors = actorsDirectorsCodesDict[key]["self"].ToHashSet();
+                    }
+                    ///
+                    if (actorsDirectorsCodesDict[key].ContainsKey("director"))
+                    {
+                        directors = actorsDirectorsCodesDict[key]["director"].ToHashSet();
+                    }
+                } 
+                if (tagCodesDict.ContainsKey(key))
+                {
+                    tags = filmTagsDict[key].ToHashSet();
+                }
                 ans1.Add(item,
                     new Movie()
                     {
-                        Title = item,
-                        Rating = raitingDict[key],
-                        Actors = actorsDirectorsCodesDict[key]["actors"].ToHashSet(),
-                        Directors = actorsDirectorsCodesDict[key]["director"].ToHashSet(),
-                        Tags = filmTagsDict[key].ToHashSet()
-                    });
+                        Title = title,
+                        Rating = raiting,
+                        Actors = actors,
+                        Directors = directors,
+                        Tags = tags
+                    }); ;
             }
         }
+
+        var t = 5;
+        
     }
 }
